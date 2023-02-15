732d19b2e065cd9670326592ce2f33aaebc38488
Ali Asghar <56997518+AliAsgharWork@users.noreply.github.com>
2023-02-15 14:41:41 +0000

    Initial Setup

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+// import HEPCA_Logo from "./HEPCA_favicon.png";
+// import HEPCA_Logo from "./HEPCA_Logo_with_name.png";
+// import HEPCA_Logo from "./Logos/2.png";
+// import HEPCA_Logo from "./Logos/3.png";
+// import HEPCA_Logo from "./Logos/4.png";
+// import HEPCA_Logo from "./Logos/5.png";
+// import HEPCA_Logo from "./Logos/6.png";
+// import HEPCA_Logo from "./Logos/7.png";
+// import HEPCA_Logo from "./Logos/logo.png";
+import HEPCA_Logo from "./Logos/logo2.png";
+import HEPCA_Logo_Favicon from "./Logos/8.png";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import research from "./research.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  HEPCA_Logo,
+  HEPCA_Logo_Favicon,
+  quotes,
+  robot,
+  research,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
