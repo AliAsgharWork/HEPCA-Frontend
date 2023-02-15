048651ee5dbc26f32f6c461c60ba5f3c9bff8d2d
Ali Asghar <56997518+AliAsgharWork@users.noreply.github.com>
2023-02-15 15:43:25 +0000

    Added Routing for Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,60 @@
-import styles from "./style";
-import {
-  Billing,
-  Business,
-  CardDeal,
-  Clients,
-  CTA,
-  Footer,
-  Navbar,
-  Stats,
-  Testimonials,
-  Hero,
-} from "./components";
+// import styles from "./style";
+// import {
+//   Billing,
+//   Business,
+//   CardDeal,
+//   Clients,
+//   CTA,
+//   Footer,
+//   Navbar,
+//   Stats,
+//   Testimonials,
+//   Hero,
+// } from "./components";
 
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
+// const App = () => (
+//   <div className="bg-primary w-full overflow-hidden">
+//     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+//       <div className={`${styles.boxWidth}`}>
+//         <Navbar />
+//       </div>
+//     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
+//     <div className={`bg-primary ${styles.flexStart}`}>
+//       <div className={`${styles.boxWidth}`}>
+//         <Hero />
+//       </div>
+//     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        {/* <Billing /> */}
-        {/* <CardDeal /> */}
-        <Testimonials />
-        {/* <Clients /> */}
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
-);
+//     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+//       <div className={`${styles.boxWidth}`}>
+//         <Stats />
+//         <Business />
+//         <Billing />
+//         <CardDeal />
+//         <Testimonials />
+//         <Clients />
+//         <CTA />
+//         <Footer />
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
